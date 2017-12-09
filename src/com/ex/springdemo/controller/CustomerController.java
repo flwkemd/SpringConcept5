34547bbebc84da77b18ef1e6258399f2aacef75d
flwkemd <flwkemd@naver.com>
2017-12-09 22:58:18 +0100

    Spring MVC and Hibernate Project

--- src/com/ex/springdemo/controller/CustomerController.java
+++ src/com/ex/springdemo/controller/CustomerController.java
@@ -1,15 +1,32 @@
 package com.ex.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
+import com.ex.springdemo.dao.CustomerDAO;
+import com.ex.springdemo.entity.Customer;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+	// need to inject the customer dao
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
+		
+		// get customers from the dao
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		
+		// add the customers to the model
+		theModel.addAttribute("customers", theCustomers);
 		
 		return "list-customers";
 	}
---
+++ src/com/ex/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.ex.springdemo.dao;
+
+import java.util.List;
+
+import com.ex.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+}
